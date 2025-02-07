b34f91186c3d46eef9e7ca6118c51f31ca2f16d4
Victor Ose Iyayi <veektaw@gmail.com>
2025-02-08 00:50:22 +0100

    Made more fixes

--- src/header.jsx
+++ src/header.jsx
@@ -1,7 +1,9 @@
+import cookImage from "./assets/cook.png";
+
 export default function Header() {
   return (
     <header>
-      <img src="./src/assets/cook.png" />
+      <img src={cookImage} alt="Cook It Logo" />
       <h1>Cook It</h1>
     </header>
   );
