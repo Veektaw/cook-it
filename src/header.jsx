88c4a84fc5508abac05eff996fb3d4ec635559bf
Victor Ose Iyayi <veektaw@gmail.com>
2025-02-08 01:35:11 +0100

    Added new icon

--- src/header.jsx
+++ src/header.jsx
@@ -1,10 +1,10 @@
-import cookImage from "./assets/cook.png";
+import cookImage from "./assets/icon.png";
 
 export default function Header() {
   return (
     <header>
       <img src={cookImage} alt="Cook It Logo" />
-      <h1>Cook It</h1>
+      <h1>Cook it</h1>
     </header>
   );
 }
